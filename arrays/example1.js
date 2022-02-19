f7271eaa6e0d8b8b813845e3d6826aa18f521691
marees <mareesvlpt@gmail.com>
2022-02-20 00:12:33 +0530

    Comments update

--- arrays/example1.js
+++ arrays/example1.js
@@ -1,6 +1,9 @@
 
-// Index is there or not. Return boolean.
+// Check whether the Index is there or not. Return boolean.
 const first = 2 in [1, 2];
-const second = '2' in [0, 1, 2];
+const second = 2 in [0, 1, 1];
+const third = '2' in [0, 1, 1];
 
-console.log(first, second);
+console.log(first); // false. because index 2 is not present in the array.
+console.log(second); // true. because index 2 is present in the array.
+console.log(third); // true. because index 2 is present in the array.
