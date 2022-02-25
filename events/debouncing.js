d69749d6b67b4ee0a0bb940fd1214d084f09de69
marees <mareesvlpt@gmail.com>
2022-02-25 14:09:26 +0530

    events | Added debouncing concept

--- events/debouncing.js
+++ events/debouncing.js
@@ -13,6 +13,7 @@ const getData = () => {
 // this debounce method will avoid unnecessary getData(API) call;
 // Only make call if the difference between the time interval between two function call is greater than the delay.
 // Only if we give pause while typing the API call will happen.
+// Creates a debounced function that delays invoking func until after wait milliseconds have elapsed since the last time the debounced function was invoked.
 const debounce = (func, delay) => {
     let timer;
     return function () {
